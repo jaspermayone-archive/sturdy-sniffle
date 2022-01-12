d89891d640c68f3bdc113551b1db30ac56e953cb
Jasper Mayone <jasper@jaspermayone.com>
2022-01-11 21:50:05 -0500

    :bug: Fixing notifications and more progress

--- Views/ContentView.swift
+++ Views/ContentView.swift
@@ -1,82 +1,70 @@
 import SwiftUI
-import CoreData
-import CareKit
-import CareKitUI
-import CareKitStore
+import SwiftUICharts
+import Indicators
 import HealthKit
-import HealthKitUI
 
 let healthStore = HKHealthStore()
 
-
 struct ContentView: View {
     var body: some View {
-        VStack {
-            ZStack {
-                Circle()
-                    .fill(Color.teal)
-                    .frame(width: 500, height: 480)
-                VStack {
-                    HStack {
-                        Image(systemName: "tray.circle.fill")
-                            .font(.system(size: 40, weight:  .semibold))
-                            .foregroundColor(.white)
-                            .padding(.trailing, 20)
-                        Text("Welcome")
-                            .foregroundColor(.white)
-                            .font(.system(size: 30, weight:  .semibold))
-                            .frame(alignment: .center)
-                        Text("User!")
-                            .foregroundColor(.white)
-                            .font(.system(size: 30, weight:  .semibold))
-                            .frame(alignment: .center)
-                        Image(systemName: "person.crop.circle.fill")
-                            .font(.system(size: 40, weight:  .semibold))
-                            .foregroundColor(.white)
-                            .padding(.leading, 20)
-                    }
-                    .padding(.top, 130)
-                    .padding(.bottom)
-                    VStack {
-                        HStack {
-                            Image(systemName: "figure.walk.circle.fill")
-                                .font(.system(size: 30, weight:  .regular))
-                                .foregroundColor(.white)
-                            Text("Total Steps:")
-                                .foregroundColor(.white)
-                                .font(.system(size: 20, weight:  .regular))
-                            Text("DATA")
-                                .foregroundColor(.red)
-                                .font(.system(size: 20, weight:  .light)).italic()
-                        }
-                        HStack {
-                            Image(systemName: "heart.circle.fill")
-                                .font(.system(size: 30, weight:  .regular))
-                                .foregroundColor(.white)
-                            Text("Heart Rate:")
-                                .foregroundColor(.white)
-                                .font(.system(size: 20, weight:  .regular))
-                            Text("DATA")
-                                .foregroundColor(.red)
-                                .font(.system(size: 20, weight:  .light)).italic()
-                        }
-                        HStack {
-                            Image(systemName: "bed.double.circle.fill")
-                                .font(.system(size: 30, weight:  .regular))
-                                .foregroundColor(.white)
-                            Text("Sleep:")
-                                .foregroundColor(.white)
-                                .font(.system(size: 20, weight:  .regular))
-                            Text("DATA")
-                                .foregroundColor(.red)
-                                .font(.system(size: 20, weight:  .light)).italic()
-                        }
-                    }
-                    .frame(alignment: .leading)
-                }
-                .padding(.top, 40)
+        if HKHealthStore.isHealthDataAvailable() {
+            VStack {
+RequestHealthAccessView()
             }
-            .padding(.top, -555)
+            //            } else {
+            //                VStack {
+            //                    ZStack {
+            //                        Circle()
+            //                            .fill(Color.teal)
+            //                            .frame(width: 500, height: 400)
+            //                        VStack {
+            //                            HStack {
+            //                                Image(systemName: "tray.circle.fill")
+            //                                    .font(.system(size: 40, weight:  .semibold))
+            //                                    .foregroundColor(.white)
+            //                                Text("Welcome")
+            //                                    .foregroundColor(.white)
+            //                                    .font(.system(size: 30, weight:  .semibold))
+            //                                    .frame(alignment: .center)
+            //                                Text("User!")
+            //                                    .foregroundColor(.white)
+            //                                    .font(.system(size: 30, weight:  .semibold))
+            //                                    .frame(alignment: .center)
+            //                                Image(systemName: "person.crop.circle.fill")
+            //                                    .font(.system(size: 40, weight:  .semibold))
+            //                                    .foregroundColor(.white)
+            //                            }
+            //                            .padding(.top, 130)
+            //                            .padding(.bottom)
+            //                        }
+            //                        .padding(.top, 10)
+            //                    }
+            //                    .padding(.top, -555)
+            //
+            //                    VStack {
+            //                        //       LineChart(chartData: LineChartData)
+            //                    }
+            //
+            //                }
+            //
+            //            }
+            
+        } else {
+            HStack {
+                Image(systemName: "exclamationmark.triangle.fill")
+                    .foregroundColor(.red)
+                    .font(.system(size: 40, weight:  .medium))
+                
+                Text("NOT AVAIBLE ON THIS DEVICE")
+                    .foregroundColor(.red)
+                    .font(.system(size: 30, weight:  .medium))
+                
+                Image(systemName: "exclamationmark.triangle.fill")
+                    .foregroundColor(.red)
+                    .font(.system(size: 40, weight:  .medium))
+                
+            }
+            .padding()
         }
     }
 }
---
+++ Views/WelcomeScreenView.swift
@@ -0,0 +1,18 @@
+import SwiftUI
+import CareKitUI
+import CareKit
+import CareKitStore
+
+struct WelcomeScreenView: View {
+    
+    var body: some View {
+        VStack {
+        }
+    }
+}
+
+struct WelcomeScreenView_Previews: PreviewProvider {
+    static var previews: some View {
+        WelcomeScreenView()
+    }
+}
